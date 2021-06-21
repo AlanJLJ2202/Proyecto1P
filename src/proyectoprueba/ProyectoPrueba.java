6843ce02055825bdb28b1fa54aa64c221d79b8a2
AlanJLJ2202 <lrs19110248@purisima.tecnm.mx>
2021-06-20 20:38:47 -0500

    Prueba commit

---
+++ src/proyectoprueba/ProyectoPrueba.java
@@ -0,0 +1,15 @@
+
+package proyectoprueba;
+
+/**
+ *
+ * @author AJLJ-
+ */
+public class ProyectoPrueba {
+
+    
+    public static void main(String[] args) {
+         System.out.println("Hola mundo :D");
+    }
+    
+}
